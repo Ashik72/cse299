e1670c991a9bdb6de55e1f31bb9b77daee30ff1e
Ashiqur Rahman <ashik72@Ashiqurs-MacBook-Pro.local>
2018-11-10 14:43:57 +0600

    initial

---
+++ api/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/hello', function () {
+   return "hello";
+});
+
+$router->get('/hello/{name}', function ($name) {
+    return "hello ".$name;
+});
+
+
+$router->get('/db', function () {
+    dd(app('db'));
+    return DB::select("SELECT * FROM departments");
+});
